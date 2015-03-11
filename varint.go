b855da5df22f68c00f279a90e2149ce09ee22faa
yourihua <yourihua@gmail.com>
2015-03-11 15:41:08 +0800

    add get uvarint and varint size func

---
+++ varint.go
@@ -0,0 +1,18 @@
+package link
+
+func UvarintSize(x uint64) int {
+	i := 0
+	for x >= 0x80 {
+		x >>= 7
+		i++
+	}
+	return i + 1
+}
+
+func VarintSize(x int64) int {
+	ux := uint64(x) << 1
+	if x < 0 {
+		ux = ^ux
+	}
+	return UvarintSize(ux)
+}
