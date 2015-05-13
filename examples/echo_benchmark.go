fa30e0c2bac588bee94fd15f6cf89c4b76fc9083
idada <bg5sbk@gmail.com>
2015-05-13 17:01:24 +0800

    压力测试工具加入多进程模式

--- examples/echo_benchmark.go
+++ examples/echo_benchmark.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"github.com/funny/link"
 	"github.com/funny/link/protocol/fixhead"
+	"io"
 	"net"
+	"os/exec"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -16,6 +20,8 @@ var (
 	messageSize = flag.Int("size", 64, "test message size")
 	runTime     = flag.Int("time", 10, "benchmark run time in seconds")
 	asyncChan   = flag.Int("async", 10000, "async send chan size, 0 == sync send")
+	proces      = flag.Int("procs", 1, "how many benchmark process")
+	waitMaster  = flag.Bool("wait", false, "DO NOT USE")
 )
 
 type ClientResult struct {
@@ -23,6 +29,14 @@ type ClientResult struct {
 	ReadCount  int
 	WriteCount int
 }
+
+type childProc struct {
+	Cmd    *exec.Cmd
+	Stdin  io.WriteCloser
+	Stdout bytes.Buffer
+}
+
+const OutputFormat = "Send Count: %d, Total Size: %d, Read Count: %d, Write Count: %d\n"
 
 // This is an benchmark tool work with the echo_server.
 //
@@ -37,6 +51,10 @@ type ClientResult struct {
 //     go run echo_benchmark.go -addr="127.0.0.1:10010"
 func main() {
 	flag.Parse()
+
+	if MultiProcess() {
+		return
+	}
 
 	var (
 		msg        = link.Bytes(make([]byte, *messageSize))
@@ -75,8 +93,7 @@ func main() {
 		writeCount += result.WriteCount
 	}
 
-	fmt.Printf(
-		"Send Count: %d, Total Size: %d, Read Count: %d, Write Count: %d\n",
+	fmt.Printf(OutputFormat,
 		sendCount, sendCount*(*messageSize), readCount, writeCount,
 	)
 }
@@ -148,4 +165,84 @@ func client(initWait *sync.WaitGroup, conn *CountConn, startChan chan int, resul
 		conn.ReadCount,
 		conn.WriteCount,
 	}
+}
+
+func MultiProcess() bool {
+	if *proces <= 1 {
+		if *waitMaster {
+			fmt.Scanln()
+		}
+		return false
+	}
+
+	cmds := make([]*childProc, *proces)
+	for i := 0; i < *proces; i++ {
+		cmd := exec.Command(
+			"go",
+			"run",
+			"echo_benchmark.go",
+			"addr="+*serverAddr,
+			"num="+strconv.Itoa(*clientNum / *proces),
+			"size="+strconv.Itoa(*messageSize),
+			"time="+strconv.Itoa(*runTime),
+			"async="+strconv.Itoa(*asyncChan),
+			"wait",
+		)
+		stdin, err := cmd.StdinPipe()
+		if err != nil {
+			panic("get stdin pipe failed: " + err.Error())
+		}
+		cmds[i] = &childProc{Cmd: cmd, Stdin: stdin}
+		cmd.Stdout = &cmds[i].Stdout
+		cmd.Start()
+	}
+
+	for i := 0; i < *proces; i++ {
+		cmds[i].Stdin.Write([]byte{'\n'})
+	}
+
+	for i := 0; i < *proces; i++ {
+		err := cmds[i].Cmd.Wait()
+		if err != nil {
+			println("wait proc failed:", err.Error())
+		}
+	}
+
+	var (
+		totalSendCount  = 0
+		totalTotalSize  = 0
+		totalReadCount  = 0
+		totalWriteCount = 0
+	)
+
+	for i := 0; i < *proces; i++ {
+		output := cmds[i].Stdout.String()
+		fmt.Print(output)
+
+		var (
+			sendCount  = 0
+			totalSize  = 0
+			readCount  = 0
+			writeCount = 0
+		)
+
+		fmt.Sscanf(output, OutputFormat,
+			&sendCount,
+			&totalSize,
+			&readCount,
+			&writeCount,
+		)
+
+		totalSendCount += sendCount
+		totalTotalSize += totalSize
+		totalReadCount += readCount
+		totalWriteCount += writeCount
+	}
+
+	fmt.Println("--------------------")
+	fmt.Printf(OutputFormat,
+		totalSendCount, totalTotalSize, totalReadCount, totalWriteCount,
+	)
+
+	return true
 }
